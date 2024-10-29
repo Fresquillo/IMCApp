bd879858e4412958b534ad69256d7cff5f90346f
rafmorcar2 <rafamc999@gmail.com>
2024-10-29 14:01:53 +0100

    Acabar en casa (Avanzado)

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -3,8 +3,11 @@ package com.example.imcapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.internal.ViewUtils.getBackgroundColor
 
 class ImcCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,51 @@ class ImcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+         lateinit var viewMale: CardView
+         lateinit var viewFemale: CardView
+
+        fun initComponents() {
+            viewMale = findViewById(R.id.viewMale)
+            viewFemale = findViewById(R.id.viewFemale)
+        }
+
+        initComponents()
+        initListeners()
+        initUI(setGenderColor())
+
+        fun initListeners() {
+            viewMale.setOnClickListener {
+                changeGender()
+                setGenderColor()
+                viewMale.setCardBackgroundColor()
+
+                viewFemale.setOnClickListener{
+                    changeGender()
+                    setGenderColor()
+                    viewFemale.setCardBackgroundColor()
+
+                }
+            }
+
+        }
+
+        fun setGenderColor() {
+            viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+            viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
+        }
+
+        fun getBackgroundColor(isComponentSelected: Boolean): Int {
+            val colorReference = if (isComponentSelected) {
+                R.color.bg_comp_sel
+            } else {
+                R.color.bg_comp
+            }
+            return ContextCompat.getColor(this, colorReference)
+        }
+
+        fun initUI() {
+
+        }
     }
 }
+
